9c86b7ce66ce93d9aec3bdde2ae94d01b1c44f55
Eric Jacobson <erj826@bu.edu>
2018-07-06 14:40:01 +0000

    Now uses threads instead of processes

--- watchEndpoints.py
+++ watchEndpoints.py
@@ -10,36 +10,35 @@
 
 import itertools
 import resources.rabbitListener as listener
-from resources.eventQueue import eventQueue
 from yaml import load
-from multiprocessing import Process, Manager
-from flask import Flask
+from flask import Flask, request
+from threading import Thread
+from collections import deque
 
 
 app = Flask(__name__)
+pathToYamlConfig = 'config.yml'
 
 
 @app.route('/')
 def needsEndpoint():
     """No endpoint given"""
-    return "No endpoint specified in the URL!\n"
+    return 'No endpoint specified in the URL!\n'
 
 
 @app.route('/<endpoint>')
 def addQueueToDict(endpoint):
     """Initialize an empty queue for a client in the shared dictionary"""
     if endpoint in endpoints:
-        #Following line isn't doing anything. Should be appending a queue to the list 
-        #D[endpoint].append(eventQueue())
-        #print D
-        return "added event queue to %s key\n" % endpoint
+        D[endpoint].append(deque())
+        return 'added event queue to %s key\n' % endpoint
     else:
-        return "Invalid endpoint!\n"
+        return 'Invalid endpoint!\n'
 
 
 def readYaml():
     """Returns a list of endpoints from config.yml"""
-    config = load(file('config.yml', 'r'))
+    config = load(file(pathToYamlConfig, 'r'))
     listsOfEndpoints = [config[service] for service in config]
     endpoints = list(itertools.chain.from_iterable(listsOfEndpoints))
     return endpoints
@@ -48,15 +47,13 @@ def readYaml():
 def initializeDict(endpoints):
     """Initializes the shared dictionary to store endpoint data"""
     for endpoint in endpoints:
-        D[endpoint] = manager.list()
+        D[endpoint] = []
 
 
 if __name__ == "__main__":
     worker = listener.createListener()
     endpoints = readYaml()
-    manager = Manager()
-    D = manager.dict()
+    D = {}
     initializeDict(endpoints)
-    startListener = Process(target=worker.run).start()
+    startListener = Thread(target=worker.run).start()
     app.run()
-

26757a0270e47986e7192f0941b242162907da72
Eric Jacobson <erj826@bu.edu>
2018-08-21 14:25:58 +0000

    Removed deprecated authentication methods

--- user.py
+++ user.py
@@ -25,18 +25,9 @@ args = parser.parse_args()
 
 
 def main():
-    #Get token
-    try:
-        token = subprocess.check_output(shlex.split('openstack token issue -f yaml'))
-        projectID = yaml.load(token)['project_id']
-    except Exception as e:
-        print "Error: %s" % e
-        print "Curling without token..."
-        projectID = 'emptyToken'
-
     #Curl app server
     try:
-        query = appURL + '/' + args.endpoint + '/' + projectID
+        query = appURL + '/' + args.endpoint
         subprocess.call(shlex.split('curl %s' % query))
     except KeyboardInterrupt as e:
         print '\nClosing connection...'

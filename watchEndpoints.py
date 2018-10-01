1b9fc3bafc2cc8b6afc63a7d29ad27b2f65c56e6
Eric Jacobson <erj826@bu.edu>
2018-10-01 13:50:05 +0000

    minor updates before trying to switch keystone libs

--- watchEndpoints.py
+++ watchEndpoints.py
@@ -17,6 +17,21 @@ from flask import Flask, Response
 from resources.client import Client
 from werkzeug.contrib import fixers
 from oslo_config import cfg
+from oslo_config import types
+
+
+#Configure oslo
+PortType = types.Integer(1, 65535)
+
+common_opts = [
+    cfg.StrOpt('bind_host',
+               default='0.0.0.0',
+               help='IP address to listen on.'),
+    cfg.Opt('bind_port',
+            type=PortType,
+            default=9292,
+            help='Port number to listen on.')
+]
 
 
 #Initialize app with or without Keystone Authentication
